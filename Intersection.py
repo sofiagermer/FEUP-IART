558cfc91c36e9aeff8617f218ea081e7b4b8b9fb
Sergio Estevao <smestevao11@gmail.com>
2022-03-19 16:46:56 +0000

    added move function to Car

--- Intersection.py
+++ Intersection.py
@@ -1,2 +1,15 @@
 class Intersection:
-    pass
+    def __init__(self, id):
+        """
+        :param id: unique id of the intersection
+        """
+        self.id = id
+        self.incoming = {}
+        self.outgoing = {}
+        self.light = None
+
+    def add_incoming(self, street):
+        self.incoming[street.name] = street
+
+    def add_outgoing(self, street):
+        self.outgoing[street.name] = street
